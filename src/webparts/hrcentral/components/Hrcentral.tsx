da3204975be6e16b6f11877ca044851d09ac042e
IncySolutions <incysolutions@gmail.com>
2022-01-26 18:27:22 +0530

    Bootstrap Incuded

--- src/webparts/hrcentral/components/Hrcentral.tsx
+++ src/webparts/hrcentral/components/Hrcentral.tsx
@@ -6,110 +6,24 @@ require('./Styles/customCSS.css');
 
 
 export default class Hrcentral extends React.Component<IHrcentralProps, {}> {
-  public render(): React.ReactElement<IHrcentralProps> {
-    return (
-<div id="Web_1366__1">
-	<svg className="Rectangle_1">
-		<rect id="Rectangle_1" rx="30" ry="30" x="0" y="0" width="204" height="96">
-		</rect>
-	</svg>
-	<svg className="Rectangle_2">
-		<rect id="Rectangle_2" rx="30" ry="30" x="0" y="0" width="204" height="96">
-		</rect>
-	</svg>
-	<svg className="Rectangle_3">
-		<rect id="Rectangle_3" rx="30" ry="30" x="0" y="0" width="204" height="96">
-		</rect>
-	</svg>
-	<svg className="Rectangle_4">
-		<rect id="Rectangle_4" rx="30" ry="30" x="0" y="0" width="204" height="96">
-		</rect>
-	</svg>
-	<div id="Onboard">
-		<a href="#">Onboardqq</a>
-	</div>
-	<div id="Onboard_l">
-		<span>Onboard</span>
-	</div>
-	<div id="Onboard_m">
-		<span>Onboard</span>
-	</div>
-	<div id="Onboard_n">
-		<span>Onboard</span>
-	</div>
-	<img id="Image_1" src={require('../assets/Image_1.png')}></img>
-		
-	<img id="Image_1_p" src={require('../assets/Image_1_p.png')}></img>
-		
-	<img id="Image_1_q" src={require('../assets/Image_1_q.png')}></img>
-		
-	<img id="Image_1_r" src={require('../assets/Image_1_r.png')}></img>
-		
-	<svg className="Rectangle_5">
-		<rect id="Rectangle_5" rx="17" ry="17" x="0" y="0" width="254" height="59">
-		</rect>
-	</svg>
-	<img id="Ellipse_1" src={require('../assets/Ellipse_1.png')}></img>
-		
-	<div id="AMISH_RAJ">
-		<span>AMISH RAJ</span>
-	</div>
-	<div id="HR_Head">
-		<span>HR Head</span>
-	</div>
-	<svg className="Rectangle_6">
-		<rect id="Rectangle_6" rx="17" ry="17" x="0" y="0" width="514" height="59">
-		</rect>
-	</svg>
-	<div id="Search">
-		<span>Search</span>
-	</div>
-	<img id="Image_1_y" src={require('../assets/Image_1_y.png')}></img>
-		
-	<svg className="Ellipse_2">
-		<ellipse id="Ellipse_2" rx="466" ry="428" cx="466" cy="428">
-		</ellipse>
-	</svg>
-	<div id="HR_CENTRAL">
-		<span>HR CENTRAL</span>
-	</div>
-	<div id="Resource_management_at_a_singl">
-		<span>Resource management at a single place</span>
-	</div>
-	<div id="Lorem_ipsum_dolor_sit_amet_con">
-		<span>Lorem ipsum dolor sit amet, consectetur<br/>adipiscing elit. Sit lorem tincidunt volutpat<br/>st, enim. Nulla sit et euismod vitae interdum fames.<br/>Porttitor id viverra ipsum morbi.</span>
-	</div>
-	<div id="Icon_feather-arrow-left-circle">
-		<svg className="Path_1" viewBox="3 3 62 62">
-			<path id="Path_1" d="M 65 33.99999618530273 C 65 51.12082290649414 51.12082290649414 65 33.99999618530273 65 C 16.87917137145996 65 2.999999761581421 51.12082290649414 2.999999761581421 33.99999618530273 C 2.999999761581421 16.87917137145996 16.87917137145996 2.999999761581421 33.99999618530273 2.999999761581421 C 51.12082290649414 2.999999761581421 65 16.87917137145996 65 33.99999618530273 Z">
-			</path>
-		</svg>
-		<svg className="Path_2" viewBox="12 12 12.4 24.8">
-			<path id="Path_2" d="M 24.39999771118164 11.99999904632568 L 11.99999904632568 24.39999771118164 L 24.39999771118164 36.79999923706055">
-			</path>
-		</svg>
-		<svg className="Path_3" viewBox="12 18 24.8 3">
-			<path id="Path_3" d="M 36.79999923706055 18 L 11.99999904632568 18">
-			</path>
-		</svg>
-	</div>
-	<div id="Icon_feather-arrow-left-circle_">
-		<svg className="Path_1_" viewBox="3 3 62 62">
-			<path id="Path_1_" d="M 65 33.99999618530273 C 65 51.12082290649414 51.12082290649414 65 33.99999618530273 65 C 16.87917137145996 65 2.999999761581421 51.12082290649414 2.999999761581421 33.99999618530273 C 2.999999761581421 16.87917137145996 16.87917137145996 2.999999761581421 33.99999618530273 2.999999761581421 C 51.12082290649414 2.999999761581421 65 16.87917137145996 65 33.99999618530273 Z">
-			</path>
-		</svg>
-		<svg className="Path_2_" viewBox="12 12 12.4 24.8">
-			<path id="Path_2_" d="M 24.39999771118164 11.99999904632568 L 11.99999904632568 24.39999771118164 L 24.39999771118164 36.79999923706055">
-			</path>
-		</svg>
-		<svg className="Path_3_ba" viewBox="12 18 24.8 3">
-			<path id="Path_3_ba" d="M 36.79999923706055 18 L 11.99999904632568 18">
-			</path>
-		</svg>
-	</div>
-	<img id="v25_3722x" src={require('../assets/v25_3722x.png')}></img>
-</div>
-
-    );
-  }
+	public render(): React.ReactElement<IHrcentralProps> {
+		return (
+			<div id="Web_1366__1">
+				<nav className="navbar navbar-default">
+					<div className="container-fluid">
+						<div className="navbar-header">
+							<a className="navbar-brand" href="#">WebSiteName</a>
+						</div>
+						<ul className="nav navbar-nav">
+							<li className="active"><a href="#">Home</a></li>
+							<li><a href="#">Page 1</a></li>
+							<li><a href="#">Page 2</a></li>
+							<li><a href="#">Page 3</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+			
+		);
+	}
 }
